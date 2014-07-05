5edc6515e9781354a0347a97a62f71854d048039
Scott Fleckenstein <nullstyle@gmail.com>
2014-07-05 16:03:53 -0700

    Start to move towards chai

--- index.js
+++ index.js
@@ -3,7 +3,16 @@ var stexDev = module.exports;
 stexDev.gulp        = require("./lib/gulp");
 stexDev.gulpPlugins = require("./lib/gulp-plugins");
 stexDev.should      = require("should");
+stexDev.chai        = require("chai");
 stexDev.supertest   = require("supertest");
+
+stexDev.chai.use(require('chai-properties'));
+
+stexDev.supertest.Test.prototype.expectBody = function(body) {
+  return this.expect(function(res) {
+    stexDev.chai.expect(res.body).to.have.properties(body);
+  });
+};
 
 stexDev.paths = {
   "lint":  ['./gulpfile.js', './lib/**/*.js', './config/**/*.js', './migrations/**/*.js'],
