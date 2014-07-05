b9a44885612da6232c987a3ec69b8a23473b8f52
Scott Fleckenstein <nullstyle@gmail.com>
2014-07-05 08:28:50 -0700

    Import lint and mocha into stex-dev

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 var stexDev = module.exports;
 
+stexDev.gulp        = require("./lib/gulp");
+stexDev.gulpPlugins = require("./lib/gulp-plugins");
+stexDev.should      = require("should");
+stexDev.supertest   = require("supertest");
 
-stexDev.gulp = require("./lib/gulp")
+stexDev.paths = {
+  "lint":  ['./gulpfile.js', './lib/**/*.js', './config/**/*.js', './migrations/**/*.js'],
+  "watch": ['./gulpfile.js', './lib/**', './test/**/*.js', '!test/{temp,temp/**}'],
+  "tests": ['./test/**/*.js', '!test/{temp,temp/**}']
+};
