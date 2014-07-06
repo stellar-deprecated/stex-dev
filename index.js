ecdf76970c3d97db35c85eab9c2a14ee79114bc6
Scott Fleckenstein <nullstyle@gmail.com>
2014-07-05 20:40:04 -0700

    Adds support file loader, moves chai and supertest initialization into support

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
 var stexDev = module.exports;
 
-stexDev.gulp        = require("./lib/gulp");
-stexDev.gulpPlugins = require("./lib/gulp-plugins");
-stexDev.chai        = require("chai");
-stexDev.supertest   = require("supertest");
+stexDev.gulp             = require("./lib/gulp");
+stexDev.gulpPlugins      = require("./lib/gulp-plugins");
+stexDev.loadSupportFiles = require("./lib/load-support-files");
 
-stexDev.chai.use(require('chai-properties'));
-
-stexDev.supertest.Test.prototype.expectBody = function(body) {
-  return this.expect(function(res) {
-    stexDev.chai.expect(res.body).to.have.properties(body);
-  });
-};
+stexDev.loadSupportFiles(__dirname + "/lib");
 
 stexDev.paths = {
   "lint":  ['./gulpfile.js', './lib/**/*.js', './config/**/*.js', './migrations/**/*.js'],
